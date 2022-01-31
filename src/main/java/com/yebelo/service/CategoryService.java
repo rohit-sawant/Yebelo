552a4e321e3a11b864d54607b94ee2b137030938
rohit-sawant <sawant.rohit510@gmail.com>
2022-01-31 23:12:52 +0530

    adding request's in controller

--- src/main/java/com/yebelo/service/CategoryService.java
+++ src/main/java/com/yebelo/service/CategoryService.java
@@ -6,36 +6,38 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.yebelo.entity.Category;
 import com.yebelo.helper.Helper;
 import com.yebelo.repository.CategoryRepository;
 
+@Service
 public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
-	 Category saveCategory(Category category) {
+	 public Category saveCategory(Category category) {
 		return this.categoryRepository.save(category);
 	}
 	 
-	 List<Category> saveAllCategory(List<Category> categories) {
+	 public List<Category> saveAllCategory(List<Category> categories) {
 		 return this.categoryRepository.saveAll(categories);
 	 }
 	 
-	 void deleteCategoryById(int id) {
+	 public void deleteCategoryById(int id) {
 		  this.categoryRepository.deleteById(id);
 	 }
 	 
-	 Optional<Category> getCategoryById(int id) {
+	 public Optional<Category> getCategoryById(int id) {
 		 return this.categoryRepository.findById(id);
 	 }
 	 
-	 List<Category> getAllCategory(){
+	 public List<Category> getAllCategory(){
 		 return this.categoryRepository.findAll();
 	 }
 	 
-	 ResponseEntity<?> fetchNextNumber(Category c){
+	 public ResponseEntity<?> fetchNextNumber(Category c){
 		 try {
 
 			 Category category = this.categoryRepository.findByCategoryCode(c.getCategoryCode());
@@ -45,7 +47,7 @@ public class CategoryService {
 			 return ResponseEntity.ok(Map.of("old value",oldValue,"new value",category.getValue()));
 		 }
 		 catch (Exception e) {
-			 
+			 e.printStackTrace();
 		}
 		 return ResponseEntity.ok(Map.of("message","error!! something went wrong","status","error"));
 	 }
